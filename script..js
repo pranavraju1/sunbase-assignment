ea527ee176821cc28780d26dc1e9967a56222d92
Pranav Raju <pranavraju2020@gmail.com>
2024-02-21 10:54:34 +0530

    final comments

--- script..js
+++ script..js
@@ -38,7 +38,7 @@ function createInputItem(labelText) {
   label.innerText = labelText;
 
   const button = document.createElement("button");
-  button.className = "delete"
+  button.className = "delete";
   button.innerText = "Delete";
 
   details.appendChild(label);
@@ -46,13 +46,15 @@ function createInputItem(labelText) {
   inputItem.appendChild(details);
 
   const input = document.createElement("input");
-  if (labelText === "Sample Textarea") {                  //for textarea
+  if (labelText === "Sample Textarea") {
+    //for textarea
     const textarea = document.createElement("textarea");
     textarea.cols = "20";
     textarea.rows = "10";
     textarea.placeholder = "Please enter in textarea";
     inputItem.appendChild(textarea);
-  } else if (labelText === "Sample Select") {             //for select dropdown
+  } else if (labelText === "Sample Select") {
+    //for select dropdown
     const select = document.createElement("select");
     for (let i = 0; i < 3; i++) {
       const option = document.createElement("option");
@@ -61,7 +63,8 @@ function createInputItem(labelText) {
       select.appendChild(option);
     }
     inputItem.appendChild(select);
-  } else {                                                //for input text
+  } else {
+    //for input text
     input.placeholder = "Please enter";
     input.type = "text";
     inputItem.appendChild(input);
@@ -75,89 +78,100 @@ function createInputItem(labelText) {
     inputItem.classList.remove("dragging");
   });
 
-  button.addEventListener("click", () => {                //delete button
+  button.addEventListener("click", () => {
+    //delete button
     inputItem.remove();
   });
 
   return inputItem;
 }
 
-main.addEventListener("dragover", (e) => {              //element drop place logic
+main.addEventListener("dragover", (e) => {
+  //element drop place logic
   e.preventDefault();
   const afterElement = getDragAfterElement(main, e.clientY);
   console.log(afterElement);
   const draggable = document.querySelector(".dragging");
   if (afterElement == null) {
-    main.appendChild(draggable);                        //if not existing place at end
+    main.appendChild(draggable); //if not existing place at end
   } else {
-    main.insertBefore(draggable, afterElement);         //else place before
+    main.insertBefore(draggable, afterElement); //else place before
   }
 });
 
-function getDragAfterElement(main, y) {                 //logic to get next element while dragging
+function getDragAfterElement(main, y) {
+  //logic to get next element while dragging
   const draggableElements = [
-    ...main.querySelectorAll(".input-item:not(.dragging)"),
+    ...main.querySelectorAll(".input-item:not(.dragging)"), //elements not being dragged
   ];
   return draggableElements.reduce(
+    //to find the one closest to current mouse position
     (closest, child) => {
+      //closest is the accumulator and child are the elements
       const box = child.getBoundingClientRect();
+      // DOMRect object providing information about the size of an element and its position relative to the viewport.
       // console.log(box);
-      const offset = Math.floor(y - box.top - box.height / 2);
+      const offset = Math.floor(y - box.top - box.height / 2); //calculating the distance b/w mouse and center of child
       console.log(offset);
-      if (offset < 0 && offset > closest.offset) {      //if less than zero element hovering on elemnts abovve
+      if (offset < 0 && offset > closest.offset) {
+        //if less than zero element hovering on elemnts abovve
         return { offset: offset, element: child };
-      } else {                                          //else element is hovering on elemnts below
+      } else {
+        //else element is hovering on elemnts below
         return closest;
       }
     },
     { offset: Number.NEGATIVE_INFINITY }
+    //closest initial value is negative infinity this is done to ensure that
+    // the  first draggable element encountered will always be considered closer initially.
   ).element;
 }
 
 form.addEventListener("click", (e) => {
   e.preventDefault();
   let inputItems = document.querySelectorAll(".input-item");
-  if(inputItems.length === 0){                          //checking if any element is added in form
+  if (inputItems.length === 0) {
+    //checking if any element is added in form
     alert("Add Form Elements");
     return;
   }
   let obj = [];
-  inputItems.forEach((item,index) => {                //preparing json output
+  inputItems.forEach((item, index) => {
+    //preparing json output
     let type = item.childNodes[1].localName;
     if (type === "input") {
       obj.push({
-        id: new Date().getTime()+index,
+        id: new Date().getTime() + index,
         type: type,
         label: item.childNodes[0].firstChild.innerText,
-        placeholder:item.childNodes[1].placeholder,
-        value: item.childNodes[1].value
+        placeholder: item.childNodes[1].placeholder,
+        value: item.childNodes[1].value,
       });
-    }else if(type === "select"){
-      let selectOptions=[];
-      item.childNodes[1].childNodes.forEach((element)=>{
+    } else if (type === "select") {
+      let selectOptions = [];
+      item.childNodes[1].childNodes.forEach((element) => {
         selectOptions.push(element.innerText);
-      })
+      });
       obj.push({
-        id: new Date().getTime()+index,
+        id: new Date().getTime() + index,
         type: type,
         label: item.childNodes[0].firstChild.innerText,
         options: selectOptions,
-        value: item.childNodes[1].value
+        value: item.childNodes[1].value,
       });
-    }else if (type === "textarea") {
+    } else if (type === "textarea") {
       obj.push({
-        id: new Date().getTime()+index,
+        id: new Date().getTime() + index,
         type: type,
         label: item.childNodes[0].firstChild.innerText,
-        placeholder:item.childNodes[1].placeholder,
-        value: item.childNodes[1].value
+        placeholder: item.childNodes[1].placeholder,
+        value: item.childNodes[1].value,
       });
-    }    
+    }
   });
-  let displayDetails = document.querySelector(".dispaly-detail") ;
+  let displayDetails = document.querySelector(".dispaly-detail");
   console.log(obj);
   const objJson = JSON.stringify(obj);
   console.log(objJson);
-  displayDetails.innerText = objJson; 
-
+  displayDetails.innerText = objJson;
 });
